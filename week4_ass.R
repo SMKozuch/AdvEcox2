0a132a007cac88b801e388b7c9a80969191d2847
SMKozuch <33494811+SMKozuch@users.noreply.github.com>
2018-01-30 19:01:33 +0100

    minor

--- week4_ass.R
+++ week4_ass.R
@@ -10,20 +10,13 @@ fig <- read.dta('FIG.dta')
 
 ##### Panel regression
 fig_panel <- pdata.frame(fig, index=c('country', 'period'))
-panel_regression <- plm(growth ~ (lly + btot + privo) + dby + log(initial) + log(initial) * lly + log(initial) * btot + 
-                          log(initial) * privo + log(initial) * dby, data=fig_panel)
+
+normal_formula = formula(growth ~ (lly + btot + privo + dby) + log(initial) + log(initial) * (lly + btot + privo + dby))
+extended_formula = formula(growth ~ (lly + btot + privo + dby) + log(initial) + log(initial) * (lly + btot + privo + dby) +
+                             school + gov + log1p(pi) + log1p(bmp))
+
+panel_regression <- plm(normal_formula, data=fig_panel)
 summary(panel_regression)
 
-panel_regression <- plm(growth ~ (lly + btot + privo) + dby + log(initial) + log(initial) * lly + log(initial) * btot + 
-                          log(initial) * privo + log(initial) * dby + school + gov + log1p(pi) + log1p(bmp), data=fig_panel)
-
-betas <- coef(panel_regression)
-
-lly_fitted <- fig_panel[,'lly'] * betas[1]
-btot_fitted <- fig_panel[,'btot'] * betas[2]
-privo_fitted <- fig_panel[,'privo'] * betas[3]
-
-growth_fitted <- lly_fitted + btot_fitted + privo_fitted
-
-resettest(growth ~ log(lly) + log(btot) + log(privo) + log(dby), power = 2:5, data = fig_panel, type = c('fitted'))
-
+panel_regression_control <- plm(extended_formula, data=fig_panel)
+summary(panel_regression_control)
